8d7239cf27223dff94825be459790e6115f42ebd
nikhil arora <narora200@gmail.com>
2022-07-01 11:49:34 +0530

    feat: setting up Raffle smart contract

---
+++ contracts/Raffle.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+error Raffle_NotEnoughEthEntered();
+
+contract Raffle {
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle_NotEnoughEthEntered();
+        }
+        s_players.push(payable(msg.sender));
+    }
+
+    // function pickRandomWinner() {}
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+}
