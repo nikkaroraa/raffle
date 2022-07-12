5241fb230cb5e9b8dd0c420b35ea569d51657225
nikhil arora <narora200@gmail.com>
2022-07-12 23:26:51 +0530

    feat: deploy script

---
+++ deploy/00-deploy-mocks.js
@@ -0,0 +1,26 @@
+const { developmentChains } = require("../helper-hardhat-config")
+const { ethers } = require("hardhat")
+
+const BASE_FEE = ethers.utils.parseEther("0.25") // 0.25 is the premium. It costs 0.25 LINK per request
+const GAS_PRICE_LINK = 1e9
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const args = [BASE_FEE, GAS_PRICE_LINK]
+
+    if (developmentChains.includes(network.name)) {
+        log("Local network detected! Deploying mocks...")
+
+        // deploy a mock vrfCoordinator...
+        await deploy("VRFCoordinatorV2Mock", {
+            from: deployer,
+            log: true,
+            args,
+        })
+        log("Mocks deployed!")
+        log("------------------------------")
+    }
+}
+
+module.exports.tags = ["all", "mocks"]
